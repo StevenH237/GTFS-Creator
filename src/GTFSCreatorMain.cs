606da99b09213b049ecd02ead859d7fa1d8ea267
Steven H <github@steven-h.net>
2021-12-08 12:49:41 -0500

    A little progress

--- src/GTFSCreatorMain.cs
+++ src/GTFSCreatorMain.cs
@@ -1,5 +1,30 @@
-namespace Nixill.GTFS {
-  public class GTFSCreatorMain {
-    
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using OsmSharp;
+using OsmSharp.Streams;
+
+// GTFS Creator: From an OSM-XML output of a transit network and a schedule CSV, 
+namespace Nixill.GTFS
+{
+  public class GTFSCreatorMain
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to the GTFS creator");
+
+      // Select the OSM file
+      Console.WriteLine("Select the OSM file containing route information (leave blank to quit): ");
+      string file = Console.ReadLine();
+      if (file == null || file == "") return;
+
+      // Start parsing the file
+      using var stream = File.OpenRead(file);
+      using var source = new XmlOsmStreamSource(stream);
+
+      // For now let's just get all the components. I can optimize this later.
+      Dictionary<long, Node> nodes = source.Where(x => x.Type == OsmGeoType.Node).ToDictionary(x => x.Id.Value, x => (Node)x);
+    }
   }
 }
